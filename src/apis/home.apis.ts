a7cd794d90a34c06d8baf1176968300d52b2f128
keykhanh20032003 <kellezs14linnie@gmail.com>
2024-11-05 12:34:56 +0700

    Đóng gói source code vào folder FE_USER để dễ quản lí

--- src/apis/home.apis.ts
+++
@@ -1,8 +0,0 @@
-import httpRequest from "~/constants/httpRequest";
-import { Banner } from "~/types/category.type";
-import { SuccessResponse } from "~/types/utils.type";
-
-const homeApi = {
-  getSlideHome : () => httpRequest.get<SuccessResponse<Banner>, any>(`/api/banner`),
-}
-export default homeApi;
